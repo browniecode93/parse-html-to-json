5db62f265e05d6b903fa53ffba0882fc7205a53e
Farzaneh <farzaneh@Farzanehs-MacBook-Pro.local>
2021-11-08 23:59:39 +0330

    using b4 to extract dict keys

--- main.py
+++ main.py
@@ -2,34 +2,47 @@ import json
 import xmltojson
 
 from pathlib import Path
-
+from bs4 import BeautifulSoup
 
 def read_files_name(root):
-    file_names = []
     # Read all directories on data
     directories = [item for item in Path(root).iterdir() if Path(item).is_dir()]
     # Create file name
     for dir in directories:
+        file_names = []
+        print(f'Read files on {dir}')
         for f in Path(dir).iterdir():
-            file_names.append(f'{str(dir)}/{f.name}')
-
-    return file_names
+            if f.suffix == '.html':
+                file_names.append(f'{str(dir)}/{f.name}')
+        if str(dir) != 'lot-parser/data/2017-12-20':
+            print(dir)
+            html2json(file_names)
 
 def html2json(file_names):
+    content = []
     for file in file_names:
-        file_obj = Path(file)
-        if file_obj.stat().st_size != 0:
+        works = []
+        # Check if file is empty
+        if Path(file).stat().st_size != 0:
             with open(file, "r") as html_file:
                 html = html_file.read()
-                json_ = xmltojson.parse(html)
-                json2file(f'{str(file_obj.parent)}/{file_obj.stem}.json', json_)
-        else:
-            print(f'File {file_obj.name} is empty!')
+                soup = BeautifulSoup(html, "html.parser")
+                works.append({
+                    "title": soup.find('h3').get_text(),
+                    "price": soup.find_all('div')[1].get_text()
+                })
+                info = {
+                    "artist": soup.find('h2').get_text(),
+                    "works": works
+                }
+                content.append(info)
+
+    json2file(f'{str(Path(file).parent)}/result.json', content)
 
 def json2file(file_name, content):
     with open(file_name, "w") as file:
         json.dump(content, file)
 
+
 if __name__ == "__main__":
     file_names = read_files_name('lot-parser/data/')
-    html2json(file_names)
