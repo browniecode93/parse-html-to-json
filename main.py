6342359c2696a0044aab5a4f10902da0baff56a2
Farzaneh <farzaneh@Farzanehs-MacBook-Pro.local>
2021-11-08 20:59:48 +0330

    rm ds files, read file names

--- main.py
+++ main.py
@@ -4,11 +4,23 @@ import xmltojson
 from pathlib import Path
 
 
-def read_file():
-    return
+def read_file_name(root):
+    file_name = []
+    # Read all directories on data
+    directories = [item for item in Path(root).iterdir()]
+    # Create file name
+    for dir in directories:
+        for f in Path(dir).iterdir():
+            file_name.append(f'{str(dir)}/{f.name}')
+
+    return file_name
 
 def html2json():
     return
 
 def print_json():
     return
+
+if __name__ == "__main__":
+    file_name = read_file_name('lot-parser/data/')
+    print(file_name)
